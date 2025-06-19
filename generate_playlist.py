8209125e814279d0a401b75f3048113eb649f703
Sven Hajer <sven.hajer@gmail.com>
2025-06-19 23:25:13 +0200

    Update generate_playlist.py

--- generate_playlist.py
+++ generate_playlist.py
@@ -6,7 +6,7 @@ from spotipy.oauth2 import SpotifyOAuth
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="DEINE_CLIENT_ID",
     client_secret="DEIN_CLIENT_SECRET",
-    redirect_uri="http://127.0.0.1:8888/callback"
+    redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-library-read user-read-recently-played playlist-modify-private playlist-modify-public"
 ))
 
